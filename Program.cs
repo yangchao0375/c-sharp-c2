9f9197a8d5877c12d597842e530adb34e1a95853
3253099643qq.com <3253099643@qq.com>
2021-03-22 17:48:51 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_c2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            DateTime datetime = new DateTime();
+            datetime = DateTime.Now.AddSeconds(5);
+            clock.SetAlarmTime(datetime);
+            clock.Start();
+        }
+    }
+}
